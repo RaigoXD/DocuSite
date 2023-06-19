73596b7c6c7c7fc806b7fde7c379372ada104dc0
JuanOrozco03 <juanorozco0310@gmail.com>
2023-06-19 16:31:17 -0500

    Crear cuenta vista

--- Docusite_Frontend/src/app/login/components/create-account/create-account.component.ts
+++ Docusite_Frontend/src/app/login/components/create-account/create-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-account',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateAccountComponent implements OnInit {
 
-  constructor() { }
+createForm!: FormGroup;
+
+  constructor(
+    public formBuilder: FormBuilder
+  ) { }
+
+  buildForm(): FormGroup {
+    return this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      name: ['', Validators.required],
+      lastName: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
+    this.createForm = this.buildForm();
   }
 
 }
